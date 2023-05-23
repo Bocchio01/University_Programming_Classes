9586dcca18af8ecb8d794b807e08c50e6e7acf94
Bocchio01 <tommaso.bocchietti@gmail.com>
2023-05-23 23:56:16 +0200

    Fixed data reshape + Added support for negative NATOMS

--- 2023 - Sperimentazione e Analisi dei Dati/Main.m
+++ 2023 - Sperimentazione e Analisi dei Dati/Main.m
@@ -4,11 +4,11 @@ clear variables
 
 %% Requests
 
-% Scrivere uno script Matlab che, dato un file molecola.cube, scrive un file sistema.xyz rispettando le seguenti condizioni:
-% 1. il file molecola.cube contiene la densità elettronica di una molecola qualsiasi;
+% Scrivere uno script Matlab che, dato un file molecola.cube', ..., scrive un file sistema.xyz rispettando le seguenti condizioni:
+% 1. il file molecola.cube', ... contiene la densità elettronica di una molecola qualsiasi;
 % 2. il file sistema.xyz contiene la struttura del sistema formato da (i) la molecola considerata al punto precedente (cioè quella di cui è data la densità elettronica) e (ii) un atomo di litio;
 % 3. nel file sistema.xyz, l’atomo di litio è posto in un punto scelto casualmente tra tutti quelli con densità elettronica ρ(x, y, z) = ρ0 ± δρ;
-% 4. lo script permette all’utente di controllare direttamente i valori di ρ0 e δρ.
+% 4. lo script permette all utente di controllare direttamente i valori di ρ0 e δρ.
 
 % In aggiunta, è possibile estendere lo script per fare in modo che possa essere posizionato un numero qualsiasi di atomi di litio in maniera analoga a quella appena esposta. In questo caso devono sussistere le seguenti condizioni:
 % 1. lo script permette all’utente di scegliere quanti atomi di litio posizionare;
@@ -18,13 +18,19 @@ clear variables
 
 %% Section 0: Variable declaration
 
-% cube_file_name = 'Cube files/cr-h2o_6-orb-40.cube';
-% cube_file_name = 'Cube files/cr-h2o_6-dens-dubl.cube';
-% cube_file_name = 'Cube files/h2o-elf.cube';
-% cube_file_name = 'Cube files/h2o-elpot.cube';
-cube_file_name = 'Cube files/h2o-nopot-dens.cube';
-% cube_file_name = 'Cube files/au-dens-2.cube';
-% cube_file_name = 'densita.cube';
+clear rho_0 delta_rho N_lithium R_lithium;
+
+names = {'Cube files/au-dens-0.cube', ...
+    'Cube files/cr-h2o_6-dens-diff.cube', ...
+    'Cube files/cr-h2o_6-dens-dubl.cube', ...
+    'Cube files/cr-h2o_6-orb-40.cube', ...
+    'Cube files/h2o-dens.cube', ...
+    'Cube files/h2o-elf.cube', ...
+    'Cube files/h2o-elpot.cube', ...
+    'Cube files/h2o-nopot-dens.cube', ...
+    'densita.cube'};
+
+cube_file_name = string(names(1));
 
 rho_0 = 0.3;
 delta_rho = 0.05;
@@ -34,29 +40,32 @@ R_lithium  = 0.3;
 
 graph = true;
 
+if (~exist('rho_0', 'var') || ~exist('delta_rho', 'var'))
+    rho_0 = input('Enter the value for density ρ0: ');
+    delta_rho = input('Enter the value of the tolerance δρ: ');
+end
+
+if (~exist('N_lithium', 'var') || ~exist('R_lithium', 'var'))
+    N_lithium = input('Enter the number of lithium atoms to be added: ');
+    R_lithium = input('Enter the value of the atom radius: ');
+end
+
 
 %% Section 1: Reading molecule state data
 
-% Open the .cube file
+% Open the .cube', ... file
 fid = fopen(cube_file_name,'r');
 
-% Skip the first two lines of the file
-COMMENT1 = fgetl(fid);
-COMMENT2 = fgetl(fid);
+COMMENTS = [strtrim(fgetl(fid)) '; ' strtrim(fgetl(fid))];
 
 % Read the third line of the file and extract the number of atoms and the origin coordinates
 data = sscanf(fgetl(fid), '%d %f %f %f %d');
-if data(1) <= 0
-    disp('Negative number of athoms not supported. Exit the program')
-    return
-else
-    NATOMS = data(1);
-end
+NATOMS = data(1);
 ORIGIN = struct('X', data(2), 'Y', data(3), 'Z', data(4));
-
-% {NVAL} may be omitted if its value would be equal to one; it MUST be absent or have a value of one if {NATOMS} is negative.
-if (length(data) == 5)
-    NVAL = data(5);
+if (NATOMS > 0 && length(data) == 5)
+    DIM4 = data(5);
+else
+    DIM4 = 1;
 end
 
 % Read the next three lines of the file to extract the origin coordinates and grid spacing
@@ -68,13 +77,19 @@ end
 
 % Read the info related to the atoms presents in the moleculas
 ATOMS_INFO = struct('N', [], 'C', [], 'X', [], 'Y', [], 'Z', []);
-for n = 1:NATOMS
+for n = 1:abs(NATOMS)
     data = sscanf(fgetl(fid), '%d %f %f %f %f');
     ATOMS_INFO(n) = struct('N', data(1), ...
         'C', data(2), ...
         'X', data(3), ...
         'Y', data(4), ...
         'Z', data(5));
+end
+
+% DSET_IDS handling
+if (NATOMS < 0)
+    DIM4 = fscanf(fid, '%d', 1);
+    fscanf(fid, '%d', DIM4);
 end
 
 clear n;
@@ -84,6 +99,7 @@ clear n;
 
 % Read the rest of the file to extract the electronic density data
 data = fscanf(fid, '%f');
+
 % Close the file
 fclose(fid);
 
@@ -93,8 +109,12 @@ fclose(fid);
 VOXEL_GRID_MATRIX = [[AXIS.X]', [AXIS.Y]', [AXIS.Z]'];
 ORIGIN_VECTOR = struct2array(ORIGIN)';
 
-% Reshape the data into a 3D array
-density = reshape(data, [AXIS.N]);
+% Reshape the data into a 4D array
+density = reshape(data, AXIS(3).N, AXIS(2).N, AXIS(1).N, DIM4);
+density = permute(density, [3, 2, 1, 4]);
+
+% We consider just the first level of the fourth dimension to simplify 
+density = density(:,:,:,1);
 
 % Plot N slice of the electronic density data
 if graph
@@ -105,12 +125,14 @@ if graph
         colormap("parula");
         % colormap("hot");
         colorbar;
-        imagesc(squeeze(density(:,:,round(section_level))));
-        title(sprintf('Densità elettronica @Z=%d', round(section_level)))
+        imagesc(squeeze(density(:,:,round(section_level))'));
+        title(sprintf('Electronic density @z_{eq}=%d (/%d)', round(section_level), AXIS(3).N))
         xlabel('x_{eq}')
         ylabel('y_{eq}')
     end
 end
+
+clear section_level;
 
 
 %% Section 4: Identify iso-density surface
@@ -120,7 +142,7 @@ idx = find(abs(density - rho_0) <= delta_rho);
 [x,y,z] = ind2sub(size(density), idx);
 
 % We pass from index value to coordinate so we downgrade by 1 [x y z]
-ISO_DENSITY = ([z y x]-1) * VOXEL_GRID_MATRIX + ORIGIN_VECTOR';
+ISO_DENSITY = ([x y z]-1) * VOXEL_GRID_MATRIX + ORIGIN_VECTOR';
 ISO_DENSITY = struct('X', num2cell(ISO_DENSITY(:, 1)), ...
     'Y', num2cell(ISO_DENSITY(:, 2)), ...
     'Z', num2cell(ISO_DENSITY(:, 3)));
@@ -138,7 +160,7 @@ if graph
         'filled');
     colorbar;
     view(3);
-    title(sprintf('Volume ad iso-densità, rho=%0.3f+-%0.3f', rho_0, delta_rho))
+    title(sprintf('Iso-density volume, ρ=%0.3f±%0.3f', rho_0, delta_rho))
     xlabel('X');
     ylabel('Y');
     zlabel('Z');
@@ -151,24 +173,17 @@ if graph
     grid on
 
     % Create a 3D grid of coordinates corresponding to the density matrix
-    maxN = max([AXIS.N]);
-    xyz_vectors = ([1:maxN; 1:maxN; 1:maxN]-1)' * VOXEL_GRID_MATRIX + ORIGIN_VECTOR';
-    [X,Y,Z] = meshgrid(xyz_vectors(1:AXIS(1).N, 1), ...
+    xyz_vectors = repmat((0:max([AXIS.N]-1))', 1, 3) * VOXEL_GRID_MATRIX + ORIGIN_VECTOR';
+    [X,Y,Z] = ndgrid(xyz_vectors(1:AXIS(1).N, 1), ...
         xyz_vectors(1:AXIS(2).N, 2), ...
         xyz_vectors(1:AXIS(3).N, 3));
-    
-    permutation_order = [3, 1, 2];
-    X = permute(X, permutation_order);
-    Y = permute(Y, permutation_order);
-    Z = permute(Z, permutation_order);
-
 
     % Visualizing iso-density surface
     for isovalue = rho_0 + [-delta_rho, delta_rho]
 
         % Create the isosurface using the isodensity value and the density matrix
         fv = isosurface(X, Y, Z, ...
-            reshape(data, size(X)), ...
+            density, ...
             isovalue);
 
         % Plot the isosurface with a color map
@@ -182,21 +197,19 @@ if graph
         lighting gouraud;
 
     end
-    title(sprintf('Superfici superiore ed inferiore ad iso-densità, rho=%0.3f', rho_0))
+    title(sprintf('Upper and lower surfaces at iso-density level, ρ=%0.3f±%0.3f', rho_0, delta_rho))
     xlabel('X');
     ylabel('Y');
     zlabel('Z');
+
 end
 
+clear isovalue;
 
-%% Section 5: Random positioning of the Lithium athoms
 
-if (~exist('N_lithium', 'var') || ~exist('R_lithium', 'var'))
-    N_lithium = input('Enter the number of lithium athoms to be added: ');
-    R_lithium = input('Enter the value of the athom radius: ');
-end
+%% Section 5: Random positioning of the Lithium atoms
 
-LI_VECTOR = struct('X', [], 'Y', [], 'Z', []);
+LI_POSITION = struct('X', [], 'Y', [], 'Z', []);
 foundPosition = false(N_lithium, 1);
 maxAttempts = 20;
 
@@ -210,12 +223,12 @@ for i = 1:N_lithium
         LI = ISO_DENSITY(index);
 
         % Calculate the distances between the new atom and existing atoms
-        D = pdist([[LI_VECTOR.X]' [LI_VECTOR.Y]' [LI_VECTOR.Z]'; ...
+        D = pdist([[LI_POSITION.X]' [LI_POSITION.Y]' [LI_POSITION.Z]'; ...
             struct2array(LI)]);
 
         % Check if the distances satisfy the constraint
         if all(D >= R_lithium)
-            LI_VECTOR(i) = LI;
+            LI_POSITION(i) = LI;
             foundPosition(i) = true;
             break;
         end
@@ -236,7 +249,7 @@ view(3);
 clear i;
 
 
-%% Section 6: Write to XYZ file
+%% Section 6: Write to XYZ file and plot atoms
 
 % Load the periodical element from external file
 Elements = table2struct(readtable('periodic-table.xlsx'));
@@ -246,18 +259,18 @@ fid = fopen('sistema.xyz', 'w');
 
 % Write the number of atoms as the first line and comment as second
 fprintf(fid, '%d\n', NATOMS + nnz(foundPosition));
-fprintf(fid, '%s\n', strcat(strtrim(COMMENT1), '-', strtrim(COMMENT2)));
+fprintf(fid, '%s\n', COMMENTS);
 
-% Adding the index element value to each lithium athom added
-LI_INFO = arrayfun(@(s) struct('N', 3, 'X', s.X, 'Y', s.Y, 'Z', s.Z), LI_VECTOR);
+% Adding the index element value to each lithium atom added
+LI_INFO = arrayfun(@(s) struct('N', 3, 'X', s.X, 'Y', s.Y, 'Z', s.Z), LI_POSITION);
 
 % Write the XYZ coordinates to the file
 print_to_xyz(fid, ATOMS_INFO, Elements);
 print_to_xyz(fid, LI_INFO(foundPosition), Elements);
 
 if graph
-    plot_xyz(ATOMS_INFO, Elements, 'g');
-    plot_xyz(LI_INFO(foundPosition), Elements, 'b');
+    plot_xyz(ATOMS_INFO, Elements, 'g', true);
+    plot_xyz(LI_INFO(foundPosition), Elements, 'b', false);
 end
 
 % Close the file
@@ -279,7 +292,7 @@ end
 end
 
 
-function [] = plot_xyz(atoms_data, Elements, color)
+function [] = plot_xyz(atoms_data, Elements, color, show_name)
 for i = 1:numel(atoms_data)
     [X, Y, Z] = sphere(20);
     R = Elements(atoms_data(i).N).x_VanDerWaalsRadius__pm_/1000;
@@ -288,5 +301,13 @@ for i = 1:numel(atoms_data)
         R * Z + atoms_data(i).Z, ...
         'EdgeColor', 'none', ...
         'FaceColor', color);
+end
+
+if show_name
+    textOffset = 0.1;
+    text([atoms_data.X] + textOffset, ...
+        [atoms_data.Y] + textOffset, ...
+        [atoms_data.Z] + textOffset, ...
+        {Elements([atoms_data.N]).Symbol});
 end
 end
